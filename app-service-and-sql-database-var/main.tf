ce110537b7beb67845da415dfe9c3c65145f0817
abhishek1O5 <50949483+abhishek1O5@users.noreply.github.com>
2019-10-03 14:48:45 +0530

    done

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "test1" {
   name     = "tarraform-resouces-abhishek"
   location = "${var.location}"
 }
 
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_app_service_plan" "test1" {
   name                = "example-appserviceplan"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test1.location}"
+  resource_group_name = "${azurerm_resource_group.test1.name}"
 
   sku {
     tier = "Standard"
@@ -14,11 +14,11 @@ resource "azurerm_app_service_plan" "test" {
   }
 }
 
-resource "azurerm_app_service" "test" {
+resource "azurerm_app_service" "test1" {
   name                = "tarraform-resouces-abhi"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+  location            = "${azurerm_resource_group.test1.location}"
+  resource_group_name = "${azurerm_resource_group.test1.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test1.id}"
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -32,24 +32,24 @@ resource "azurerm_app_service" "test" {
   connection_string {
     name  = "Database"
     type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+    value = "Server=tcp:${azurerm_sql_server.test1.fully_qualified_domain_name} Database=${azurerm_sql_database.test1.name};User ID=${azurerm_sql_server.test1.administrator_login};Password=${azurerm_sql_server.test1.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
   }
 }
 
 resource "azurerm_sql_server" "test" {
   name                         = "terraform-sqlserver"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.test1.name}"
+  location                     = "${azurerm_resource_group.test1.location}"
   version                      = "12.0"
   administrator_login          = "houssem"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
-resource "azurerm_sql_database" "test" {
+resource "azurerm_sql_database" "test1" {
   name                = "terraform-sqldatabase"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  location            = "${azurerm_resource_group.test.location}"
-  server_name         = "${azurerm_sql_server.test.name}"
+  resource_group_name = "${azurerm_resource_group.test1.name}"
+  location            = "${azurerm_resource_group.test1.location}"
+  server_name         = "${azurerm_sql_server.test1.name}"
 
   tags = {
     environment = "production"
